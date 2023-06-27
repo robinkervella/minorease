29a3d3479165048cdb23fd9a23ae98d8bdc61b4f
robinkervella <121940567+robinkervella@users.noreply.github.com>
2023-06-27 09:20:39 +0200

    Initial commit

---
+++ src/main/java/fr/simplon/minorease/MinoreaseApplication.java
@@ -0,0 +1,13 @@
+package fr.simplon.minorease;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MinoreaseApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MinoreaseApplication.class, args);
+    }
+
+}
---
+++ src/test/java/fr/simplon/minorease/MinoreaseApplicationTests.java
@@ -0,0 +1,13 @@
+package fr.simplon.minorease;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MinoreaseApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
