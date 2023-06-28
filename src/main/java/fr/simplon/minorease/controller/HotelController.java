4b32b5f578c2bff0ebb22a5e14ba9062735212f7
Andy <121941283+Andytomsingvien@users.noreply.github.com>
2023-06-28 21:52:39 +0200

    HotelController fix afin de pouvoir récupérer les données. Modification de l'index(method="get" dans la balise form de la page), j'ai également commenté les 2 rechercherUnHotel() et rechercherUnHotelParPrix() dans HotelController.

--- src/main/java/fr/simplon/minorease/controller/HotelController.java
+++ src/main/java/fr/simplon/minorease/controller/HotelController.java
@@ -16,6 +16,9 @@ import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.time.LocalTime;
 import java.util.List;
+/**
+ * Classe contrôleur pour la gestion des hôtels.
+ */
 @Controller
 public class HotelController {
     @Autowired
@@ -23,27 +26,42 @@ public class HotelController {
     @Autowired
     private HotelRepository hotelRepository;
 
-    @GetMapping(path = "/Hotel/{ville}/{dateDebut}/{dateFin}/{nbPersonne}")
+    /**
+     * Recherche des hôtels en fonction des critères de l'utilisateur.
+     *
+     * @param dateDebut date de début du séjour
+     * @param dateFin date de fin du séjour
+     * @param nbPersonne nombre de personnes pour le séjour
+     * @param ville ville où l'hôtel est recherché
+     * @param model modèle pour passer des attributs à la vue
+     * @return hotels le nom de la vue à afficher
+     */
+    @GetMapping(path = "/chercherHotel")
     public String rechercherUnHotel(
             @RequestParam("dateDebut") @DateTimeFormat(pattern = "yyyy-MM-dd") LocalDate dateDebut,
             @RequestParam("dateFin") @DateTimeFormat(pattern = "yyyy-MM-dd") LocalDate dateFin,
             @RequestParam("nbPersonne") int nbPersonne,
             @RequestParam("ville") String ville,  Model model) {
-
         List<Hotel> allHotels = hotelRepository.findAll();
-
         LocalDateTime localDateTimeDebut = LocalDateTime.of(dateDebut, LocalTime.MIDNIGHT);
         LocalDateTime localDateTimeFin = LocalDateTime.of(dateFin, LocalTime.MIDNIGHT);
-
-        System.out.println("Ville: " + ville);
-        System.out.println("Date de début: " + dateDebut);
-        System.out.println("Date de fin: " + dateFin);
-        System.out.println("Nombre de personnes: " + nbPersonne);
         List<Hotel> listeARetourner = rechercheService.rechercheHotel(allHotels,ville,localDateTimeDebut,localDateTimeFin,nbPersonne);
         model.addAttribute("hotel", listeARetourner);
         return "hotels";
     }
 
+    /**
+     * Recherche des hôtels en fonction des critères de l'utilisateur, y compris le prix.
+     *
+     * @param ville ville où l'hôtel est recherché
+     * @param dateDebut date de début du séjour
+     * @param dateFin date de fin du séjour
+     * @param nbPersonne nombre de personnes pour le séjour
+     * @param prixMini prix minimum que l'utilisateur est prêt à payer
+     * @param prixMax prix maximum que l'utilisateur est prêt à payer
+     * @param model modèle pour passer des attributs à la vue
+     * @return hotels le nom de la vue à afficher
+     */
     @GetMapping(path = "/Hotel/{ville}/{dateDebut}/{dateFin}/{nbPersonne}/{prixMax}/{prixMini}")
     public String rechercherUnHotelParPrix(@RequestParam("ville") String ville, @RequestParam("dateDebut") @DateTimeFormat(iso = DateTimeFormat.ISO.DATE) LocalDate dateDebut, @RequestParam("dateFin") @DateTimeFormat(iso = DateTimeFormat.ISO.DATE) LocalDate dateFin, @RequestParam("nbPersonne") int nbPersonne,@RequestParam("prixMini")double prixMini,@RequestParam("prixMax")double prixMax, Model model) {
         List<Hotel> allHotels = hotelRepository.findAll();
