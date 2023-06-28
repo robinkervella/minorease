f4c80ca7e40149801f9d255194d00ae5a1a8c88e
Andy <121941283+Andytomsingvien@users.noreply.github.com>
2023-06-28 16:20:00 +0200

    Changement LocalDateTime en LocalDate dans HotelController

--- src/main/java/fr/simplon/minorease/controller/HotelController.java
+++ src/main/java/fr/simplon/minorease/controller/HotelController.java
@@ -4,12 +4,15 @@ import fr.simplon.minorease.entities.Hotel;
 import fr.simplon.minorease.repositories.HotelRepository;
 import fr.simplon.minorease.services.RechercheService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cglib.core.Local;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
+import java.time.LocalDate;
 import java.time.LocalDateTime;
+import java.time.LocalTime;
 import java.util.List;
 @Controller
 public class HotelController {
@@ -19,17 +22,22 @@ public class HotelController {
     private HotelRepository hotelRepository;
 
     @GetMapping(path = "/Hotel/{ville}/{dateDebut}/{dateFin}/{nbPersonne}")
-    public String rechercherUnHotel(@PathVariable String ville, @PathVariable LocalDateTime dateDebut, @PathVariable LocalDateTime dateFin, @PathVariable int nbPersonne, Model model) {
+    public String rechercherUnHotel(@PathVariable String ville, @PathVariable LocalDate dateDebut, @PathVariable LocalDate dateFin, @PathVariable int nbPersonne, Model model) {
         List<Hotel> allHotels = hotelRepository.findAll();
-        List<Hotel> listeARetourner = rechercheService.rechercheHotel(allHotels,ville,dateDebut,dateFin,nbPersonne);
+        LocalDateTime localDateTimeDebut = LocalDateTime.of(dateDebut, LocalTime.MIDNIGHT);
+        LocalDateTime localDateTimeFin = LocalDateTime.of(dateFin, LocalTime.MIDNIGHT);
+
+        List<Hotel> listeARetourner = rechercheService.rechercheHotel(allHotels,ville,localDateTimeDebut,localDateTimeFin,nbPersonne);
         model.addAttribute("hotel", listeARetourner);
         return "hotels";
     }
 
     @GetMapping(path = "/Hotel/{ville}/{dateDebut}/{dateFin}/{nbPersonne}/{prixMax}/{prixMini}")
-    public String rechercherUnHotelParPrix(@PathVariable String ville, @PathVariable LocalDateTime dateDebut, @PathVariable LocalDateTime dateFin, int nbPersonne,double prixMax,double prixMini, Model model) {
+    public String rechercherUnHotelParPrix(@PathVariable String ville, @PathVariable LocalDate dateDebut, @PathVariable LocalDate dateFin, int nbPersonne,double prixMax,double prixMini, Model model) {
         List<Hotel> allHotels = hotelRepository.findAll();
-        List<Hotel> listeARetourner = rechercheService.rechercheHotel(allHotels,ville,dateDebut,dateFin,nbPersonne);
+        LocalDateTime localDateTimeDebut = LocalDateTime.of(dateDebut, LocalTime.MIDNIGHT);
+        LocalDateTime localDateTimeFin = LocalDateTime.of(dateFin, LocalTime.MIDNIGHT);
+        List<Hotel> listeARetourner = rechercheService.rechercheHotel(allHotels,ville,localDateTimeDebut,localDateTimeFin,nbPersonne);
         List<Hotel> listeTriéAvecPrix = rechercheService.trouverLesHotelsDansLaFourchetteDePrix(listeARetourner,prixMini,prixMax);
         model.addAttribute("hotel", listeTriéAvecPrix);
         return "hotels";
