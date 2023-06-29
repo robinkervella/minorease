48c972a81f57c69879bb1827c6dac580e2bc1a73
robinkervella <121940567+robinkervella@users.noreply.github.com>
2023-06-29 16:41:31 +0200

    Reservation Service

--- src/main/java/fr/simplon/minorease/controller/ParentController.java
+++ src/main/java/fr/simplon/minorease/controller/ParentController.java
@@ -5,27 +5,35 @@ import fr.simplon.minorease.entities.Reservation;
 import fr.simplon.minorease.repositories.HotelRepository;
 import fr.simplon.minorease.repositories.ParentRepository;
 import fr.simplon.minorease.repositories.ReservationRepository;
+import fr.simplon.minorease.services.ReservationService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
+import java.security.Principal;
 import java.util.List;
 
+@Controller
 public class ParentController {
-    private final ParentRepository parentRepository; // Import du repository Parent
-    private final ReservationRepository reservationRepository; // Import du repository Reservation
-    private final HotelRepository hotelRepository; // Import du repository Hotel
+    private final ReservationService reservationService;
 
-    // Injection des repositories via le constructeur
-    public ParentController(ParentRepository parentRepository, ReservationRepository reservationRepository, HotelRepository hotelRepository) {
+    @Autowired
+    private ParentRepository parentRepository;
+    private final ReservationRepository reservationRepository;
+    private final HotelRepository hotelRepository;
+
+    public ParentController(ReservationService reservationService, ParentRepository parentRepository, ReservationRepository reservationRepository, HotelRepository hotelRepository) {
+        this.reservationService = reservationService;
         this.parentRepository = parentRepository;
         this.reservationRepository = reservationRepository;
         this.hotelRepository = hotelRepository;
     }
+
     @GetMapping("/")
     public String afficherPageAccueil() {
         return "index";
     }
-
 
 }
--- src/main/java/fr/simplon/minorease/repositories/ReservationRepository.java
+++ src/main/java/fr/simplon/minorease/repositories/ReservationRepository.java
@@ -1,7 +1,11 @@
 package fr.simplon.minorease.repositories;
 
+import fr.simplon.minorease.entities.Parent;
 import fr.simplon.minorease.entities.Reservation;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface ReservationRepository extends JpaRepository<Reservation,Long> {
+    List<Reservation> findByParentOrderByDateDesc(Parent parent);
 }
