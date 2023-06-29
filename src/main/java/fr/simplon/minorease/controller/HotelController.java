64ff297f10578a5781813796f57797c6bf3d085c
robinkervella <121940567+robinkervella@users.noreply.github.com>
2023-06-29 11:30:13 +0200

    hotel controller

--- src/main/java/fr/simplon/minorease/controller/HotelController.java
+++ src/main/java/fr/simplon/minorease/controller/HotelController.java
@@ -4,10 +4,14 @@ import fr.simplon.minorease.entities.Hotel;
 import fr.simplon.minorease.repositories.HotelRepository;
 import fr.simplon.minorease.services.RechercheService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import java.time.LocalDateTime;
 import java.util.List;
@@ -18,6 +22,14 @@ public class HotelController {
     @Autowired
     private HotelRepository hotelRepository;
 
+    @GetMapping("/hotels")
+    public String afficherHotels(@RequestParam(defaultValue = "0") int page, Model model) {
+        int pageSize = 10; // Nombre d'hôtels par page
+        Pageable pageable = PageRequest.of(page, pageSize);
+        Page<Hotel> hotels = hotelRepository.findAll(pageable);
+        model.addAttribute("hotels", hotels);
+        return "hotels";
+    }
     @GetMapping(path = "/Hotel/{ville}/{dateDebut}/{dateFin}/{nbPersonne}")
     public String rechercherUnHotel(@PathVariable String ville, @PathVariable LocalDateTime dateDebut, @PathVariable LocalDateTime dateFin, @PathVariable int nbPersonne, Model model) {
         List<Hotel> allHotels = hotelRepository.findAll();
--- src/main/java/fr/simplon/minorease/controller/ParentController.java
+++ src/main/java/fr/simplon/minorease/controller/ParentController.java
@@ -1,10 +1,31 @@
 package fr.simplon.minorease.controller;
 
+import fr.simplon.minorease.entities.Parent;
+import fr.simplon.minorease.entities.Reservation;
+import fr.simplon.minorease.repositories.HotelRepository;
+import fr.simplon.minorease.repositories.ParentRepository;
+import fr.simplon.minorease.repositories.ReservationRepository;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
+import java.util.List;
 
 public class ParentController {
+    private final ParentRepository parentRepository; // Import du repository Parent
+    private final ReservationRepository reservationRepository; // Import du repository Reservation
+    private final HotelRepository hotelRepository; // Import du repository Hotel
+
+    // Injection des repositories via le constructeur
+    public ParentController(ParentRepository parentRepository, ReservationRepository reservationRepository, HotelRepository hotelRepository) {
+        this.parentRepository = parentRepository;
+        this.reservationRepository = reservationRepository;
+        this.hotelRepository = hotelRepository;
+    }
     @GetMapping("/")
     public String afficherPageAccueil() {
         return "index";
     }
+
+
 }
